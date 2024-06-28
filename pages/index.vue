44fad0fe4dc1f682ca40b6af6358a015221f6d3a
MESSIASSANTIAGO <messiassantiago18@gmail.com>
2024-06-28 12:03:36 -0300

    secund commiit

--- pages/index.vue
+++ pages/index.vue
@@ -3,24 +3,31 @@
     <div class="flex w-full justify-center items-center mt-1 ">
         <div class="w-full h-20">
           <Carousel
-        class="relative  max-w-sm border"
+        class="relative  max-w-sm border h-full "
         :opts="{
           align: 'start',
         }"
       >
-        <CarouselContent class="h-10 flex justify-center items-center">
+        <CarouselContent class=" flex  items-center">
           <CarouselItem v-for="(_, index) in 20" :key="index" class=" max-sm:basis-1/4 md:basis-1/2 lg:basis-1/3">
             <div class="">
               <Card class="">
-                <CardContent class="flex justify-center border items-center">
-                  <span class=" font-semibold ">{{ index  +1 + "asdadsasdas"}}</span>
+                <CardContent class="flex  border items-center">
+                  <span class=" font-semibold ">{{ index  +1 + ""}}</span>
                
                 </CardContent>
               </Card>
             </div>
           </CarouselItem>
         </CarouselContent>
-       
+     
+          
+          <CarouselPrevious class="" />
+          <CarouselNext />
+
+      
+
+        
       </Carousel>
 
         </div>
